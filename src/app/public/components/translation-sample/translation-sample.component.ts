8a5b256343a959f2d1a63a2bcdd534498428b9c1
Angel Velasquez <avelasquezn@gmail.com>
2023-09-07 08:45:49 -0500

    feat(translation-sample): specified view elements.

--- src/app/public/components/translation-sample/translation-sample.component.ts
+++ src/app/public/components/translation-sample/translation-sample.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-translation-sample',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./translation-sample.component.css']
 })
 export class TranslationSampleComponent {
+  @Input() fullName: string = '';
 
 }
