2c427524a28bc3557b033e6f710535c300fb1751
Angel Velasquez <avelasquezn@gmail.com>
2023-09-07 08:37:21 -0500

    feat(home): added home page component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import { HomeComponent } from './public/pages/home/home.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent], imports: [BrowserModule, HttpClientModule, TranslateModule.forRoot({
+  declarations: [AppComponent, HomeComponent], imports: [BrowserModule, HttpClientModule, TranslateModule.forRoot({
     defaultLanguage: 'en', loader: {
       provide: TranslateLoader, useFactory: (createTranslateLoader), deps: [HttpClient]
     }
