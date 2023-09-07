4b9bcd13b54d64e1736068f7dd684bf19d8f10c4
Angel Velasquez <avelasquezn@gmail.com>
2023-09-07 08:51:04 -0500

    feat(language-selection): specified view elements.

--- src/app/public/components/language-selection/language-selection.component.ts
+++ src/app/public/components/language-selection/language-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-language-selection',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./language-selection.component.css']
 })
 export class LanguageSelectionComponent {
+  currentLang = 'en';
+  languages = ['en', 'es'];
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = translate.currentLang;
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
 
 }
